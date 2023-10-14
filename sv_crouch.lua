275411a3ab5f56313993aa9a2b1bc70c81c4ea90
IPIA <57289047+IP1A@users.noreply.github.com>
2023-10-14 20:12:13 +0300

    Update sv_crouch.lua

--- sv_crouch.lua
+++ sv_crouch.lua
@@ -1,4 +1,6 @@
-
+-- https://github.com/IP1A/Small-scripts/tree/main
+-- Выключает звуки ходьбы в присядку
+-- Disables footstep sounds when crouching
 local function NoSound(ply)
     if ply:Crouching() then return true end 
 end
